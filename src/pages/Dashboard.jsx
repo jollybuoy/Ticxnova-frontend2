8fc0a9b275d8d61cd5e97a87631be43aeb1f3a52
jollybuoy <jollybuoytech@gmail.com>
2025-04-17 16:41:41 -0400

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -115,7 +115,7 @@ const Dashboard = () => {
           </LineChart>
         </ResponsiveContainer>
 
-        <ResponsiveContainer width="100%" height={250}>
+        <ResponsiveContainer width="100%" height={300}>
           <PieChart>
             <Pie
               data={[
