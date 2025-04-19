8aa482afbfb3bd02161d6dff7f70dca6a7dccf83
jollybuoy <jollybuoytech@gmail.com>
2025-04-19 01:58:04 -0400

    Update msalConfig.js

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "23b268a2-08ce-4b76-b3b4-46c89ab7d254", // 👈 replace with your real ID
     authority: "https://login.microsoftonline.com/91dce830-003d-4d46-abfc-4fe21dfab134",
-    redirectUri: "https://yellow-dune-0ed10881e.6.azurestaticapps.net/auth/callback",
+    redirectUri: "https://yellow-dune-0ed10881e.6.azurestaticapps.net/",
   },
   cache: {
     cacheLocation: "localStorage",
