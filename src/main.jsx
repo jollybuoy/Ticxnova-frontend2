7c4de9841287072f67eff8228ebde5e71f4322c7
jollybuoy <jollybuoytech@gmail.com>
2025-04-18 22:08:12 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { MsalProvider } from "@azure/msal-react";
+import { msalInstance } from "./auth/msalConfig";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <MsalProvider instance={msalInstance}>
     <App />
-  </React.StrictMode>
+  </MsalProvider>
 );
