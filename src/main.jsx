244861cf25bd35a10e78a5bd8d379672f340cfc0
jollybuoy <jollybuoytech@gmail.com>
2025-04-19 01:08:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import App from "./App";
+import { msalConfig } from "./auth/msalConfig";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const msalInstance = new PublicClientApplication(msalConfig);
+
+// Wait for redirect to complete
+msalInstance.handleRedirectPromise().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </React.StrictMode>
+  );
+});
