322358e51e617994c79e875f08064e00a8956595
jollybuoy <jollybuoytech@gmail.com>
2025-04-19 01:08:11 -0400

    Create msalConfig.js

---
+++ src/auth/msalConfig.js
@@ -0,0 +1,19 @@
+import { PublicClientApplication } from "@azure/msal-browser";
+
+export const msalConfig = {
+  auth: {
+    clientId: "23b268a2-08ce-4b76-b3b4-46c89ab7d254", // 👈 replace with your real ID
+    authority: "https://login.microsoftonline.com/91dce830-003d-4d46-abfc-4fe21dfab134",
+    redirectUri: "https://yellow-dune-0ed10881e.6.azurestaticapps.net/auth/callback",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false
+  }
+};
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+export const loginRequest = {
+  scopes: ["User.Read"]
+};
