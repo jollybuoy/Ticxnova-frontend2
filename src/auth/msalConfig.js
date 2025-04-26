cdad4fdeb2458a5568b616b3ee866c997042af98
jollybuoy <jollybuoytech@gmail.com>
2025-04-26 16:50:50 -0400

    Update msalConfig.js

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -2,13 +2,13 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 export const msalConfig = {
   auth: {
-    clientId: "23b268a2-08ce-4b76-b3b4-46c89ab7d254",
-    authority: "https://login.microsoftonline.com/91dce830-003d-4d46-abfc-4fe21dfab134",
+    clientId: "23b268a2-08ce-4b76-b3b4-46c89ab7d254", // ✅ Use your actual client ID
+    authority: "https://login.microsoftonline.com/91dce830-003d-4d46-abfc-4fe21dfab134", // ✅ Your Azure tenant ID
     redirectUri: "https://yellow-dune-0ed10881e.6.azurestaticapps.net/",
   },
   cache: {
     cacheLocation: "localStorage",
-    storeAuthStateInCookie: true,
+    storeAuthStateInCookie: false
   }
 };
 
@@ -19,12 +19,6 @@ export const loginRequest = {
     "User.Read",
     "Mail.Read",
     "Mail.ReadWrite",
-    "Mail.Send",
-    "Files.Read",
-    "Files.Read.All",
-    "Sites.Read.All",
-    "User.ReadBasic.All",
-    "User.Read.All",
-    "User.ReadWrite.All",
+    "Mail.Send"
   ]
 };
