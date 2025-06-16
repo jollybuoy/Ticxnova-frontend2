80408a8d4a8d14643fa12d74de9f77f0a69904bc
jollybuoy <jollybuoytech@gmail.com>
2025-06-16 13:34:36 -0400

    Update msalConfig.js

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "23b268a2-08ce-4b76-b3b4-46c89ab7d254",
     authority: "https://login.microsoftonline.com/91dce830-003d-4d46-abfc-4fe21dfab134",
-    redirectUri: "https://ticxnova.com/", // ✅ corrected
+    redirectUri: "https://ticxnova.com/auth/callback", // ✅ corrected
   },
   cache: {
     cacheLocation: "localStorage",
